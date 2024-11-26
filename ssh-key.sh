faabfd4969badabe2274b3fe57ee28426520f91b
0xDkXy <0xdkxy.pollardlee@gmail.com>
2024-11-25 22:40:54 -0500

    update init scripts

--- ssh-key.sh
+++ ssh-key.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/bash
-NAME=$(whoami)
-if [[ $NAME != 'hl1276' ]];then
-    echo "not on remote server"
+
+REMOTE=$1
+
+if [[ -z $REMOTE ]];then
+    echo "input remote server address"
     exit
 fi
 
-#REMOTE=$1
-#
-#ssh -i "$HOME/.ssh/id_rsa_rutgers_cloudlab" $REMOTE 'mkdir /users/hl1276/.ssh'
-#
-#scp -i "$HOME/.ssh/id_rsa_rutgers_cloudlab" "$HOME/.ssh/id_rsa_rutgers_cloudlab" "$REMOTE:/users/hl1276/.ssh/"
-#ssh -i "$HOME/.ssh/id_rsa_rutgers_cloudlab" $REMOTE 'cd /users/hl1276/.ssh && git clone git@github.com:0xDkXy/cloudlab-init.git && cd cloudlab-init && ./install-pkgs.sh && ./init.sh'
+ssh -i "$HOME/.ssh/id_rsa_rutgers_cloudlab" $REMOTE 'mkdir /users/hl1276/.ssh'
+
+scp -i "$HOME/.ssh/id_rsa_rutgers_cloudlab" "$HOME/.ssh/id_rsa_rutgers_cloudlab" "$REMOTE:/users/hl1276/.ssh/"
+scp -i "$HOME/.ssh/id_rsa_rutgers_cloudlab" "./ssh.config" "$REMOTE:/users/hl1276/.ssh/config"
+
+ssh -i "$HOME/.ssh/id_rsa_rutgers_cloudlab" $REMOTE 'cd /users/hl1276/ && yes | git clone git@github.com:0xDkXy/cloudlab-init.git && cd cloudlab-init && ./install-pkgs.sh && ./init.sh'
