ea8cbd5c7bfb55796d306121bda4168438c2e1b3
0xDkXy <0xdkxy.pollardlee@gmail.com>
2024-11-25 22:32:20 -0500

    update

--- ssh-key.sh
+++ ssh-key.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/bash
+NAME=$(whoami)
+if [[ $NAME != 'hl1276' ]];then
+    echo "not on remote server"
+    exit
+fi
 
-REMOTE=$1
-
-ssh -i "$HOME/.ssh/id_rsa_rutgers_cloudlab" $REMOTE 'mkdir /users/hl1276/.ssh'
-
-scp -i "$HOME/.ssh/id_rsa_rutgers_cloudlab" "$HOME/.ssh/id_rsa_rutgers_cloudlab" "$REMOTE:/users/hl1276/.ssh/"
+#REMOTE=$1
+#
+#ssh -i "$HOME/.ssh/id_rsa_rutgers_cloudlab" $REMOTE 'mkdir /users/hl1276/.ssh'
+#
+#scp -i "$HOME/.ssh/id_rsa_rutgers_cloudlab" "$HOME/.ssh/id_rsa_rutgers_cloudlab" "$REMOTE:/users/hl1276/.ssh/"
+#ssh -i "$HOME/.ssh/id_rsa_rutgers_cloudlab" $REMOTE 'cd /users/hl1276/.ssh && git clone git@github.com:0xDkXy/cloudlab-init.git && cd cloudlab-init && ./install-pkgs.sh && ./init.sh'
