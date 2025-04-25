a0fe3e03e9d20e4bbd16f307f4371308edc63fec
0xDkXy <0xdkxy.pollardlee@gmail.com>
2025-04-25 16:58:31 -0400

    update nvim install script

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-mkdir -p $HOME/.config
-git clone git@github.com:0xDkXy/nvim.git $HOME/.config/nvim
+sudo apt update
+sudo apt install clangd clang-format ninja-build gettext cmake curl build-essential -y
 
-sudo apt install
-sudo apt-get install ninja-build gettext cmake curl build-essential -y
+mkdir -p $HOME/.config
+git clone https://github.com/0xDkXy/nvim.git $HOME/.config/nvim
+
 
 git clone https://github.com/neovim/neovim -b v0.11.0 --depth 1 /tmp/neovim
 cd /tmp/neovim
