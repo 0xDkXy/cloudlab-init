7cb919058819c29295f8cdbf35abaa8d9d371f01
0xDkXy <0xdkxy.pollardlee@gmail.com>
2025-04-28 14:02:30 -0400

    update script of neovim installation

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install clangd clang-format ninja-build gettext cmake curl build-essential -y
+sudo apt install curl clangd clang-format ninja-build gettext cmake curl build-essential -y
 
-mkdir -p $HOME/.config
-git clone https://github.com/0xDkXy/nvim.git $HOME/.config/nvim
+install_from_src () {
+    mkdir -p $HOME/.config
+    git clone https://github.com/0xDkXy/nvim.git $HOME/.config/nvim
 
 
-git clone https://github.com/neovim/neovim -b v0.11.0 --depth 1 /tmp/neovim
-cd /tmp/neovim
+    git clone https://github.com/neovim/neovim -b v0.11.0 --depth 1 /tmp/neovim
+    cd /tmp/neovim
 
-make CMAKE_BUILD_TYPE=Release
+    make CMAKE_BUILD_TYPE=Release
 
-sudo make install
+    sudo make install
+}
+
+install_from_bin () {
+    cd /tmp
+    curl -L -O https://github.com/neovim/neovim/releases/download/v0.11.1/nvim-linux-x86_64.tar.gz
+    tar -xvpf nvim-linux-x86_64.tar.gz
+    cd nvim-linux-x86_64
+    sudo chmod -R 755 .
+    sudo cp -r bin/* /usr/bin
+    sudo cp -r lib/* /usr/lib
+    sudo cp -r share/* /usr/share
+    cd /tmp
+    sudo rm -rf nvim-linux-x86_64*
+}
+
+install_from_src
