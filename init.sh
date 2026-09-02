82fc2c6ef9658d17061796dd50f5aca59480e014
0xDkXy <0xdkxy.pollardlee@gmail.com>
2026-09-02 13:58:25 -0400

    add install_codex in init

--- init.sh
+++ init.sh
@@ -4,6 +4,13 @@ dotcp() {
     cp $1 ~/$1
 }
 
+install_codex() {
+    curl -fsSL https://chatgpt.com/codex/install.sh | sh
+    source $HOME/.bashrc
+    codex plugin marketplace add DietrichGebert/ponytail
+    codex plugin add ponytail@ponytail
+}
+
 dotcp .vimrc
 dotcp .tmux.conf
 dotcp .gitconfig
@@ -13,3 +20,5 @@ cp ./ssh.config ~/.ssh/config
 
 echo 'set -o vi' >> ~/.bashrc
 echo 'export EDITOR=nvim' >> ~/.bashrc
+
+install_codex
